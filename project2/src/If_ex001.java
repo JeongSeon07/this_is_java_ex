7788a720081505871896f8a067283a4ba945f598
Jung Sun <bbh3032@naver.com>
2022-05-13 00:10:54 +0900

    첫번째 테스트

---
+++ project2/src/If_ex001.java
@@ -0,0 +1,24 @@
+
+public class If_ex001 {
+
+	public static void main(String[] args) {
+		
+				 int score = 93;
+				 
+				 if(score>=90) {
+					 System.out.println("점수가 90보다 큽니다.");
+					 System.out.println("등급은 A입니다.");
+				 }//if
+				 
+				 if (score < 90) 
+					System.out.println("점수가 90보다 낮습니다.");		//중괄호 없는 경우 여기까지만 실행
+				 	System.out.println("등급은 B입니다.");
+				 
+				 
+			}
+
+		}
+
+	
+
+
