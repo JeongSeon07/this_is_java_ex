b98a2efe6854adf11d3df6b1613c696aead06552
Jung Sun <bbh3032@naver.com>
2022-05-15 22:22:42 +0900

    반복문, 조건문 예제

---
+++ project2/src/exercise07.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class exercise07 {
+
+	public static void main(String[] args) {
+
+		boolean run = true;
+		
+		int balance = 0;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		while(run) {
+			System.out.println("------------------------------------------");
+			System.out.println("1.예금 | 2.출금 | 3.잔고 | 4.종료");
+			System.out.println("------------------------------------------");
+			System.out.print("선택> ");
+			
+			int num = scanner.nextInt();
+			
+			switch (num) {
+			case 1:
+				System.out.print("예금액> ");
+				int money = scanner.nextInt();
+				balance = balance + money;
+				break;
+			case 2:
+				System.out.print("출금액> ");
+				money = scanner.nextInt();
+				balance = balance - money;
+				break;
+			case 3:
+				System.out.print("잔고> " + balance);
+				break;
+			case 4: 
+				run = false;
+				break;
+			}
+		}//while
+		System.out.println("프로그램 종료");
+		scanner.close();
+	}//main
+
+}//class
